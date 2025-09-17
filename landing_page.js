929b9ec9744e09eee929b588190ffaafa986e64d
RickMdrs <134642504+RickMdrs@users.noreply.github.com>
2025-09-17 11:40:11 -0300

    Add files via upload

---
+++ landing_page.js
@@ -0,0 +1,6 @@
+// Botão de ação
+document.getElementById("cta").addEventListener("click", () => {
+    // Aqui você pode trocar o link para seu grupo ou página
+    window.location.href = "https://chat.whatsapp.com/JgDIribpZ28AINnBzQdbRl";
+  });
+  
